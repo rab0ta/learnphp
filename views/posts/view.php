9e61ecdc31c33791b1a476806307bb366551ed6e
rab0ta <aleksei.silber@tptlive.ee>
2024-10-01 10:17:39 +0300

    change view

--- views/posts/view.php
+++ views/posts/view.php
@@ -1,19 +1,23 @@
 <?php view('partials/header') ?>
 
 <main class="container">
-  <form method="GET" action="/admin/posts/view?id=<?=$post->id?>">
-    <div class="mb-3">
-    <h6>Id</h6>
-    <p name="Id" type="text" id="Id" value="<?=$post->id?>" ><?=$post->id?></p>
-    </div>
-    <div class="mb-3">
-    <h6>Title</h6>
-    <p name="Id" type="text" id="Id" value="<?=$post->title?>" ><?=$post->title?></p>
-    </div><div class="mb-3">
-    <h6>Text</h6>
-    <p name="Id" type="text" id="Id" value="<?=$post->body?>" ><?=$post->body?></p>
-    </div>
-  </form>
+  <table class="table">
+    <tbody>
+        <tr>
+            <th>ID</th>
+            <td><?=$post->id?></td>
+        </tr>
+        <tr>
+            <th>Title</th>
+            <td><?=$post->title?></td>
+        </tr>
+        <tr>
+            <th>Body</th>
+            <td><?=$post->body?></td>
+        </tr>
+    </tbody>
+  </table>
+  <a type="button" class="btn btn-primary" href="/admin/posts">Return</a>
 </main>
 
 <?php view('partials/footer') ?>
